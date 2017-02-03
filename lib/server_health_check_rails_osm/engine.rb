0673410a3aab50b365b30c5d8eabdcaecbde318a
Isaac Betesh <ibetesh@on-site.com>
2017-02-03 12:34:01 -0500

    Bypass SSL by default for health_index_path

--- lib/server_health_check_rails_osm/engine.rb
+++ lib/server_health_check_rails_osm/engine.rb
@@ -1,5 +1,18 @@
+require "server_health_check_rails/engine"
+
 module ServerHealthCheckRailsOsm
   class Engine < ::Rails::Engine
     isolate_namespace ServerHealthCheckRailsOsm
+
+    initializer "server_health_check_rails_osm.bypass_ssl_for_health_index_path" do |app|
+      # Allow access to health_index_path via HTTP.
+      app.config.ssl_options ||= {}
+      app.config.ssl_options[:redirect] ||= {}
+      original_exclude = app.config.ssl_options[:redirect][:exclude]
+      app.config.ssl_options[:redirect][:exclude] = ->(request){
+        ServerHealthCheckRails::Engine.routes.url_helpers.health_index_path == request.path ||
+          (original_exclude && original_exclude.call(request))
+      }
+    end
   end
 end
