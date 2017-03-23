15c23d131248b36efde70abe561c5e30fbdae51e
Isaac Betesh <ibetesh@on-site.com>
2017-03-23 10:21:10 -0400

    Don't load Rails components before initialization is complete

--- lib/server_health_check_rails_osm/engine.rb
+++ lib/server_health_check_rails_osm/engine.rb
@@ -8,6 +8,18 @@ module ServerHealthCheckRailsOsm
       ServerHealthCheckRailsOsm::Patches.apply_patches
     end
 
+    initializer "server_health_check_rails_osm.patch_log_subscriber" do
+      ActiveSupport.on_load(:action_controller) do
+        ActionController::LogSubscriber.prepend LogSubscriber
+      end
+    end
+
+    initializer "server_health_check_rails_osm.patch_rack_logger" do
+      ActiveSupport.on_load(:action_controller) do
+        Rails::Rack::Logger.prepend RackLogger
+      end
+    end
+
     initializer "server_health_check_rails_osm.bypass_ssl_for_health_index_path" do |app|
       # Allow access to health_index_path via HTTP.
       app.config.ssl_options ||= {}
